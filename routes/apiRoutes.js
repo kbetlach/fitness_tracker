b5130be2976abaa54c145b0c50ceca0a3d1289ab
kbetlach <kbetlach@gmail.com>
2020-01-28 15:11:29 -0600

    tweaked script sources and links. server and html routes work. need to fix api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,40 +1,39 @@
 const Workout = require("../models/workout.js");
 
-//module.exports = function(app) {
-//
-//  app.post("/api/workout", (req, res) => {
-//    body = req.body;
-//    Workout.create(body)
-//      .then(dbWorkout => {
-//        res.json(dbWorkout);
-//      })
-//      .catch(err => {
-//        res.status(400).json(err);
-//      });
-//  });
-//
-//  app.post("/api/workout/bulk", (req, res) => {
-//
-//    console.log(req.body);
-//
-//    Workout.insertMany(req.body)
-//      .then(dbWorkout => {
-//        res.json(dbWorkout);
-//      })
-//      .catch(err => {
-//        res.status(400).json(err);
-//      });
-//  });
-//
-//  app.get("/api/workout", (req, res) => {
-//    Workout.find({})
-//      .sort({ date: -1 })
-//      .then(dbWorkout => {
-//        res.json(dbWorkout);
-//      })
-//      .catch(err => {
-//        res.status(400).json(err);
-//      });
-//  });
-//
-//};
+module.exports = function(app) {
+
+  app.post("/api/workout", (req, res) => {
+    body = req.body;
+    Workout.create(body)
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+
+  app.post("/api/workout/bulk", (req, res) => {
+
+    console.log(req.body);
+
+    Workout.insertMany(req.body)
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+
+  app.get("/api/workout", (req, res) => {
+    Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+
+};
