79d5baee73e23a9445282aa711c306d5e67dfd01
kbetlach <kbetlach@gmail.com>
2020-02-06 15:03:24 -0600

    linked manifest to index. can now navigate offline, but not post

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,16 +3,21 @@ const FILES_TO_CACHE = [
     "/index.html",
     "/exercise.html",
     "/stats.html", 
-    "../javascript/api.js",
-    "../javascript/exercise.js", 
-    "../javascript/index.js",
-    "../javascript/stats.js",
-    "../javascript/workout.js", 
-    "../css/style.css"
+    "/javascript/api.js",
+    "/javascript/exercise.js", 
+    "/javascript/stats.js",
+    "/javascript/workout.js", 
+    "/service-worker.js",
+    "images/hulk.png",
+    "/css/style.css",
+    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+    "/exercise",
+    "/stats",
+    "/exercise?"
 ];
 
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
+const CACHE_NAME = "static-cache-v4";
+const DATA_CACHE_NAME = "data-cache-v4";
 
 // install
 self.addEventListener("install", function(evt) {
@@ -45,7 +50,28 @@ self.addEventListener("activate", function(evt) {
 });
 
 // fetch
-self.addEventListener("fetch", function(evt) {
+self.addEventListener("fetch", function (evt) {
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(evt.request);
+          });
+      }).catch(err => {
+        console.log(err)
+      })
+    );
+    return;
+  }
   evt.respondWith(
     caches.open(CACHE_NAME).then(cache => {
       return cache.match(evt.request).then(response => {
